604e5e6010b55b9de84a323571cf02845f67d9cc
Krishnahari P <krishnaharip707@gmail.com>
2025-07-22 21:05:08 +0530

    Updated EmployeeManagerController

--- java_emp_mgt/EmployeeManagerController.java
+++ java_emp_mgt/EmployeeManagerController.java
@@ -1,3 +1,4 @@
+package com.litmus7.employeeManager;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -5,23 +6,21 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeManagerController {
-	private static final String url="jdbc:mysql://localhost:3306/java_db";
-	private static final String username="root";
-	private static final String password="password";
+	private static final String URL="jdbc:mysql://localhost:3306/java_db";
+	private static final String USERNAME="root";
+	private static final String PASSWORD="krish@123";
 	
 	public Connection getConnection() throws SQLException{
-		return DriverManager.getConnection(url,username,password);
+		return DriverManager.getConnection(URL,USERNAME,PASSWORD);
 	}
 	
-	public List<String[]> readCsv(String filePath){
+	public List<String[]> readCSV(String filePath){
 		List<String[]> records=new ArrayList<>();
 		
 		try(BufferedReader br=new BufferedReader(new FileReader(filePath))){
@@ -32,13 +31,12 @@ public class EmployeeManagerController {
 			}
 		}
 		catch(IOException ioe) {
-			System.out.println("Error reading csv file "+ioe);
+			System.out.println("Error reading CSV file "+ioe);
 		}
 		return records;
 	}
 	public void writeToDb(List<String[]> data) {
 		String sql="insert into employee values(?,?,?,?,?,?,?,?)";
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 		
 		try(Connection connection=getConnection();
 			PreparedStatement ps=connection.prepareStatement(sql))
@@ -47,40 +45,69 @@ public class EmployeeManagerController {
 			int count=0,lineNo=1;
 			for(String[] str:data) {
 				lineNo++;
-				if(str.length!=8) {
+				if(!Validator.isValidRow(str)) {
 					System.out.println("Line "+lineNo+" skipped due to missing values. Please check csv file!");
 					continue;
 				}
 				try {
-					int emp_id=Integer.parseInt(str[0].trim());
-					if(employeeExists(connection,emp_id)) {
-						System.out.println("Employee with id "+emp_id+" already exists!");
-						System.out.println("Skipping the line "+lineNo);
+					int empId=Integer.parseInt(str[0].trim());
+					if(Validator.employeeExists(connection,empId)) {
+						System.out.println("Employee with id "+empId+" already exists! Line "+lineNo+" skipped");
 						continue;
 					}
-					String first_name=str[1].trim();
-					String last_name=str[2].trim();
-					String email=str[3].trim();
-					String phone=str[4].trim();
-					String department=str[5].trim();
-					int salary=Integer.parseInt(str[6].trim());
-					String joinDateStr=str[7].trim();			
-					LocalDate ld;
-					try {
-						ld=LocalDate.parse(joinDateStr,formatter);
+					String firstName=str[1].trim();
+					if(firstName.isEmpty()) {
+						System.out.println("First name field can't be empty. Line "+lineNo+" skipped");
+						continue;
 					}
-					catch(DateTimeParseException dte) {
-						System.out.println("Line "+lineNo+" skipped due to invalid date. Enter the date in dd-mm-yyyy format in csv "+dte);
+					if(!Validator.validateName(firstName)) {
+						System.out.println("First name must contain only letters. Line " + lineNo + " skipped");
+					    continue;
+					}
+					String lastName=str[2].trim();
+					if(lastName.isEmpty()) {
+						System.out.println("Last name field can't be empty. Line "+lineNo+" skipped");
+						continue;
+					}
+					if(!Validator.validateName(lastName)) {
+						System.out.println("Last name must contain only letters. Line " + lineNo + " skipped");
+					    continue;
+					}
+					String email=str[3].trim();
+					if(!Validator.validateEmail(email)) {
+						System.out.println("Invalid email: Line " + lineNo + " skipped");
+					    continue;
+					}
+					String phoneNo=str[4].trim();
+					if(!Validator.validatePhoneNumber(phoneNo)) {
+						System.out.println("Invalid phone number: Line " + lineNo + " skipped");
+					    continue;
+					}
+					String department=str[5].trim();
+					if(!Validator.validateName(department)) {
+						System.out.println("Department name must contain only letters. Line " + lineNo + " skipped");
+					    continue;
+					}
+				    String salaryStr=str[6].trim();
+				    String validSalary=Validator.validateSalary(salaryStr);
+				    if(validSalary==null) {
+						System.out.println("Salary must contain only digits.Line "+lineNo+" skipped");
+						continue;
+					}
+					String joinDateStr=str[7].trim();			
+					LocalDate ld=Validator.validateDate(joinDateStr);
+					if(ld==null) {
+						System.out.println("Invalid format of date. Enter the date in dd-MM-yyyy. Line "+lineNo+" skipped");
 						continue;
 					}
 					Date join_date=Date.valueOf(ld);
-					ps.setInt(1, emp_id);
-					ps.setString(2, first_name);
-					ps.setString(3, last_name);
+					ps.setInt(1, empId);
+					ps.setString(2, firstName);
+					ps.setString(3, lastName);
 					ps.setString(4, email);
-					ps.setString(5, phone);
+					ps.setString(5, phoneNo);
 					ps.setString(6, department);
-					ps.setInt(7, salary);
+					ps.setInt(7, Integer.parseInt(validSalary));
 					ps.setDate(8,join_date);
 					ps.addBatch();
 					System.out.println("Line "+lineNo+" proccessed correctly");
@@ -108,17 +135,8 @@ public class EmployeeManagerController {
 		}
 	}
 	public void writeDataToDb(String filePath) {
-		List<String[]> data=readCsv(filePath);
+		List<String[]> data=readCSV(filePath);
 		writeToDb(data);
 	}
-	public static boolean employeeExists(Connection con,int empId) throws SQLException {
-		String query="select employee_id from employee where employee_id=?";
-		try(PreparedStatement p=con.prepareStatement(query)){
-			p.setInt(1, empId);
-			try(ResultSet rs=p.executeQuery()){
-				return rs.next();
-			}
-		}
-		
-	}
+
 }
