dd0e877d6d6eaba2d78d8704faca97bb278c9535
Krishnahari P <krishnaharip707@gmail.com>
2025-07-24 12:52:17 +0530

    added util class to read data

---
+++ java_emp_mgt/com.litmus7.employeeManager.util/com/litmus7/employeeManager/util/CSVUtil.java
@@ -0,0 +1,33 @@
+package com.litmus7.employeeManager.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVUtil {
+    public static List<String[]> readCSV(String filePath) {
+        List<String[]> records = new ArrayList<>();
+        File file = new File(filePath);
+        
+        if (!file.exists() || !file.canRead() || !file.getName().toLowerCase().endsWith(".csv")){
+        	return records;
+        }
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            br.readLine(); 
+            String nextLine;
+            while ((nextLine = br.readLine()) != null) {
+                records.add(nextLine.split(","));
+            }
+        } 
+        catch (IOException ioe) {
+        	System.out.println("Error reading CSV file "+ioe);
+            ioe.printStackTrace();
+        }
+        return records;
+    }
+}
+
