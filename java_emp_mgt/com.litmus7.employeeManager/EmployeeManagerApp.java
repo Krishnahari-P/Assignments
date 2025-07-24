20c20bbac78b6fe34bdfbd67ddb66815eeb0741f
Krishnahari P <krishnaharip707@gmail.com>
2025-07-24 12:42:56 +0530

    updated EmployeeManagerApp

--- java_emp_mgt/com.litmus7.employeeManager/EmployeeManagerApp.java
+++ java_emp_mgt/com.litmus7.employeeManager/EmployeeManagerApp.java
@@ -2,7 +2,13 @@ package com.litmus7.employeeManager;
 class EmployeeManagerApp {
 	public static void  main(String[] args){	
 		String filePath="C:\\Users\\Krish\\Downloads\\employees.csv";
-		EmployeeManagerController obj=new EmployeeManagerController();
-		obj.writeDataToDb(filePath);
+		EmployeeManagerController controller=new EmployeeManagerController();
+		Response response=controller.writeDataToDb(filePath);
+		if (response.isSuccess()) {
+            System.out.println("Success: " + response.getMessage());
+        } 
+		else {
+            System.out.println("Failure: " + response.getMessage());
+        }
 	}
 }
