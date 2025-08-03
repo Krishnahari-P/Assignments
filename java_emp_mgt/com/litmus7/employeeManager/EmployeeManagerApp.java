3a6187a986c66d85739d9595d789d418f1422040
Krishnahari P <krishnaharip707@gmail.com>
2025-08-03 20:34:53 +0530

    Updated EmployeeManagerApp

--- java_emp_mgt/com/litmus7/employeeManager/EmployeeManagerApp.java
+++ java_emp_mgt/com/litmus7/employeeManager/EmployeeManagerApp.java
@@ -1,17 +1,33 @@
 package com.litmus7.employeeManager;
-
+import com.litmus7.employeeManager.controller.EmployeeManagerController;
 import com.litmus7.employeeManager.response.Response;
 
 class EmployeeManagerApp {
 	public static void  main(String[] args){	
-		String filePath="C:\\Users\\Krish\\Downloads\\employees.csv";
-		EmployeeManagerController controller=new EmployeeManagerController();
-		Response response=controller.writeDataToDb(filePath);
-		if (response.isSuccess()) {
+		String filePath="src\\com\\litmus7\\resources\\employees.csv";
+		EmployeeManagerController employeeManagerController=new EmployeeManagerController();
+		saveDataToDb(employeeManagerController,filePath);
+		getEmployeeData(employeeManagerController);
+	}
+	
+	public static void saveDataToDb(EmployeeManagerController employeeManagerController,String filePath) {
+		Response response=employeeManagerController.writeDataToDb(filePath);
+		if (response.getStatusCode()==200) {
             System.out.println("Success: " + response.getMessage());
         } 
 		else {
-            System.out.println("Failure: " + response.getMessage());
+            System.out.println("Failure: "+response.getStatusCode() +" "+ response.getMessage());
         }
+	}
+	
+	public static void getEmployeeData(EmployeeManagerController employeeManagerController) {
+		Response response= employeeManagerController.getAllEmployeeNames();
+		if(response.getStatusCode()==200) {
+			System.out.println("Name of existing employees int the database :");
+			System.out.println(response.getMessage());
+		}
+		else {
+			System.out.println(response.getMessage());
+		}
 	}
 }
