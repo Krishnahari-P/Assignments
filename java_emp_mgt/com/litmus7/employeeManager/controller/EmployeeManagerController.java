64b4fd060228280402df720fb576c3e7870a1066
Krishnahari P <krishnaharip707@gmail.com>
2025-08-05 21:08:45 +0530

    Added catch blocks to catch custom exception

--- java_emp_mgt/com/litmus7/employeeManager/controller/EmployeeManagerController.java
+++ java_emp_mgt/com/litmus7/employeeManager/controller/EmployeeManagerController.java
@@ -1,8 +1,9 @@
 package com.litmus7.employeeManager.controller;
+
 import java.io.File;
-import java.sql.SQLException;
 import java.util.List;
 
+import com.litmus7.employeeManager.exceptions.EmployeeManagerException;
 import com.litmus7.employeeManager.model.Employee;
 import com.litmus7.employeeManager.response.Response;
 import com.litmus7.employeeManager.service.EmployeeService;
@@ -12,10 +13,11 @@ public class EmployeeManagerController {
     private EmployeeService employeeService = new EmployeeService();
 
     public Response writeDataToDb(String filePath) {
-    	File file = new File(filePath);
-    	if (!file.exists() || !file.canRead() || !file.getName().toLowerCase().endsWith(".csv")){
-        	return new Response(400,"Invalid CSV file!");
+        File file = new File(filePath);
+        if (!file.exists() || !file.canRead() || !file.getName().toLowerCase().endsWith(".csv")) {
+            return new Response(400, "Invalid CSV file!");
         }
+
         try {
     
             List<String[]> employeeRecords = CSVUtil.readCSV(filePath);
@@ -36,12 +38,12 @@ public class EmployeeManagerController {
             return new Response(200, message);
 
         } 
-        catch (Exception e) {
+        catch (EmployeeManagerException e) {
             e.printStackTrace();
             return new Response(500, "An error occurred: " + e.getMessage());
         }
     }
-    
+
     public Response getAllEmployeeNames() {
         try {
             List<String> employeeNames = employeeService.getEmployeeNames();
@@ -49,12 +51,9 @@ public class EmployeeManagerController {
                 return new Response(204, "No employees found");
             }
             return new Response(200, String.join("\n", employeeNames));
-        } 
-        catch (SQLException e) {
-            e.printStackTrace();
-            return new Response(500, "Database error: " + e.getMessage());
-        }
+        }  
+        catch (EmployeeManagerException e) {
+        	return new Response(500, "Database error: " + e.getMessage());
+		}
     }
-
 }
-
