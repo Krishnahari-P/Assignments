511b8583e14966f0b083662d79d03c71f1b0a35d
Krishnahari P <krishnaharip707@gmail.com>
2025-08-06 17:04:36 +0530

    Added queries to fetch,delete and update data

--- java_emp_mgt/com/litmus7/employeeManager/constants/SQLConstants.java
+++ java_emp_mgt/com/litmus7/employeeManager/constants/SQLConstants.java
@@ -4,4 +4,7 @@ public class SQLConstants {
 	public final static String checkEmployeeExists = "SELECT employee_id FROM employee WHERE employee_id=?";
 	public final static String insertEmployee = "INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 	public final static String getEmployeeName= "SELECT first_name,last_name FROM employee";
+	public final static String getEmployeeById= "SELECT first_name,last_name,email,phone_no,department,salary,join_date FROM employee WHERE employee_id=?";
+	public final static String deleteEmployeeById="DELETE FROM EMPLOYEE WHERE employee_id=?";
+	public final static String updateEmployee="UPDATE EMPLOYEE SET first_name=?,last_name=?,email=?,phone_no=?,department=?,salary=?,join_date=? WHERE employee_id=?";
 }
