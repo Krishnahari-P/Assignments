76a27f20899a34252898d997002409e145629785
Krishnahari P <krishnaharip707@gmail.com>
2025-08-12 21:06:50 +0530

    Added main method

---
+++ retail_store_inventory_mgmnt/com/litmus7/retailstoreapp/RetailStoreApp.java
@@ -0,0 +1,48 @@
+package Assignments.retail_store_inventory_mgmnt.com.litmus7.retailstoreapp;
+
+import com.litmus7.retailstoreapp.controller.ProductController;
+import com.litmus7.retailstoreapp.dto.Clothing;
+import com.litmus7.retailstoreapp.dto.Product;
+import com.litmus7.retailstoreapp.response.Response;
+import com.litmus7.retailstoreapp.util.Comparators;
+
+public class RetailStoreApp {
+	public static void main(String[] args) {
+		ProductController productController=new ProductController();
+		Product product=new Clothing("1","Kurtha",2000,null,"Clothing","XL","Cotton");
+		addProduct(productController,product);
+		viewAllProducts(productController);
+		viewProductsByCategory(productController,"Electronics");
+		sortProductsInAscending(productController);
+		sortProductsByNames(productController);
+	}
+	public static void addProduct(ProductController productController,Product product) {
+		Response response=productController.addProduct(product);
+		
+		
+	}
+	public static void viewAllProducts(ProductController productController) {
+		Response response=productController.viewAllProducts();
+//		Print the responses
+	}
+	public static void viewProductsByCategory(ProductController productController,String category) {
+		Response response=productController.viewProductsByCategory(category);
+//      Print the responses
+	}
+	public static void sortProductsInAscending(ProductController productController) {
+		Response response=productController.sortAndDisplayProducts(Comparators.priceAscending);
+//		print the responses
+	}
+	public static void sortProductsInDescending(ProductController productController) {
+		Response response=productController.sortAndDisplayProducts(Comparators.priceDescending);
+//		print the responses
+	}
+	
+	public static void sortProductsByNames(ProductController productController) {
+		Response response=productController.sortAndDisplayProducts(Comparators.nameAscending);
+//		print the responses
+	}
+	
+	
+}
+
