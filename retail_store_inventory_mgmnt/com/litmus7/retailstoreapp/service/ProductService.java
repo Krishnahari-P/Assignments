98700d5804eaf3673b7d60ee3854a1be645d8bb3
Krishnahari P <krishnaharip707@gmail.com>
2025-08-19 16:42:57 +0530

    Added custom exception handling and business checks

--- retail_store_inventory_mgmnt/com/litmus7/retailstoreapp/service/ProductService.java
+++ retail_store_inventory_mgmnt/com/litmus7/retailstoreapp/service/ProductService.java
@@ -5,25 +5,59 @@ import java.util.List;
 
 import Assignments.retail_store_inventory_mgmnt.com.litmus7.retailstoreapp.dao.ProductDAO;
 import Assignments.retail_store_inventory_mgmnt.com.litmus7.retailstoreapp.dto.Product;
+import Assignments.retail_store_inventory_mgmnt.com.litmus7.retailstoreapp.exceptions.RetailException;
 
 public class ProductService {
 	private ProductDAO productDao=new ProductDAO();
 	
-	public boolean addProduct(Product product) {
-		return productDao.addProduct(product);
+	public boolean addProduct(Product product) throws RetailException {
+		try {
+			if(productDao.productExists(product)) {
+				throw new RetailException("Product already exists");
+			}	
+			return productDao.addProduct(product);
+		}
+		catch(Exception e) {
+//			Catch suitable exception
+			throw new RetailException("Produt addition failed",e);
+		}
 	}
 	
-	public List<Product> viewAllProducts() {
-        return productDao.getAllProducts();
+	public List<Product> viewAllProducts() throws RetailException {
+		try {
+			List<Product> products=productDao.getAllProducts();
+			return products;
+		}
+		catch(Exception e) {
+//			Catch suitable exception
+			throw new RetailException("Product fetching failed",e);
+		}
     }
 	
-	public List<Product> viewProductsByCategory(String category) {
-        return productDao.getProductsByCategory(category);
+	public List<Product> viewProductsByCategory(String category) throws RetailException {
+		try {
+			if(!productDao.categoryExists(category)) {
+				throw new RetailException("Category don't exists");
+			}
+			List<Product> products=productDao.getProductsByCategory(category);
+			return products;
+		}
+		catch(Exception e) {
+//			Catch suitable exception
+			throw new RetailException("Product fetching failed",e);
+		} 
     }
 	
-	public List<Product> sortProducts(Comparator<Product> comparator) {
-        List<Product> sortedProducts=productDao.getAllProducts();    
-        sortedProducts.sort(comparator);
-        return sortedProducts;
+	public List<Product> sortProducts(Comparator<Product> comparator) throws RetailException {
+		try {
+//			If comparator is empty, throw custom exception
+			List<Product> products=productDao.getAllProducts();    
+	        products.sort(comparator);
+	        return products;
+		}
+        catch(Exception e) {
+//			Catch suitable exception
+			throw new RetailException("Product sorting failed",e);
+        }
     }
 }
