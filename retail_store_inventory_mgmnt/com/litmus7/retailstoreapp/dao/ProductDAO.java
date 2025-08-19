f6e64f2576601bc69cf57f84217d06c3079b2a1e
Krishnahari P <krishnaharip707@gmail.com>
2025-08-19 16:41:30 +0530

    Added custom exception and new methods

--- retail_store_inventory_mgmnt/com/litmus7/retailstoreapp/dao/ProductDAO.java
+++ retail_store_inventory_mgmnt/com/litmus7/retailstoreapp/dao/ProductDAO.java
@@ -4,44 +4,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Assignments.retail_store_inventory_mgmnt.com.litmus7.retailstoreapp.dto.Product;
+import Assignments.retail_store_inventory_mgmnt.com.litmus7.retailstoreapp.exceptions.RetailException;
 
 public class ProductDAO {
 	
-	public boolean addProduct(Product product) {
+	public boolean addProduct(Product product) throws RetailException {
 		try {
-			//Write product details to a file using BufferedWriter
-		}
-		catch(Exception e) {
-			return false;
-		}
-		
-		//verify the insertion
-		
-		try {
-			//verify whether the data exists for the productId of the product
+//			Write product details to a file using BufferedWriter
+//			verify whether the data entered for the productId of the product
+//			Use productExists() method. If no, throw RetailDaoException
 			return true;
 		}
 		catch(Exception e) {
-			return false;
+			throw new RetailException("Adding product failed",e);
 		}
 	}
 	
-	public List<Product> getAllProducts(){
+	public List<Product> getAllProducts() throws RetailException{
 		List<Product> productList=new ArrayList<>();
 		try {
 			//Fetch details of all products and add to list
 		}
 		catch(Exception e) {
 			//catch proper exception and handle it 
+			throw new RetailException("Fetching failed",e);
 		}
 		return productList;
 	}
 	
-	public List<Product> getProductsByCategory(String category){
-		List<Product> productList=new ArrayList<>();
-		//Filter products by category from file
-		//add the products details to list
-		return productList;
+	public List<Product> getProductsByCategory(String category) throws RetailException{
+		try {
+			List<Product> products=new ArrayList<>();
+			//Filter products by category from file
+			//add the products details to list
+			return products;
+		}
+		catch(Exception e) {
+			throw new RetailException("Fetching failed",e);
+		}
 		
+	}
+	
+	public boolean productExists(Product product) {
+		String productId=product.getProductId();
+//		check if the productId exists
+//		If exists return true
+		return false;
+	}
+	
+	public boolean categoryExists(String category) {
+//		Check if provided category is present in the file
+//		if present return true
+		return false;
 	}
 }
