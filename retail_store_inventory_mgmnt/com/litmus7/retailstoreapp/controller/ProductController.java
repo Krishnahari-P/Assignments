5698c917217e0b26704c8716ee94cbc239582e95
Krishnahari P <krishnaharip707@gmail.com>
2025-08-19 16:44:01 +0530

    Added custom exception handling and new methods for sorting

--- retail_store_inventory_mgmnt/com/litmus7/retailstoreapp/controller/ProductController.java
+++ retail_store_inventory_mgmnt/com/litmus7/retailstoreapp/controller/ProductController.java
@@ -5,46 +5,110 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import Assignments.retail_store_inventory_mgmnt.com.litmus7.retailstoreapp.dto.Product;
+import Assignments.retail_store_inventory_mgmnt.com.litmus7.retailstoreapp.exceptions.RetailException;
 import Assignments.retail_store_inventory_mgmnt.com.litmus7.retailstoreapp.response.Response;
 import Assignments.retail_store_inventory_mgmnt.com.litmus7.retailstoreapp.service.ProductService;
 
 public class ProductController {
-	public ProductService productService=new ProductService();
+	private ProductService productService=new ProductService();
 	
-	public Response addProduct(Product product) {
-		
+	public Response addProduct(Product product) {		
 //		Perform input validations
-        boolean productAdded=productService.addProduct(product);
-        if(productAdded) {
-        	return new Response(200,"Product added");
+		try {
+			boolean productAdded=productService.addProduct(product);
+	        if(productAdded) {
+	        	return new Response(200,"Product added");
+	        }
+	        return new Response(500,"Product not added");
+		}
+        catch(RetailException e) {
+        	return new Response(504,"Product addition failed "+e.getMessage());
         }
-        return new Response(500,"Product not added");
     }
 	
 	public Response viewAllProducts() {
-		List<Product> products = productService.viewAllProducts();
-		if(products.isEmpty()) {
-			return new Response(500,"No products available");
+		try {
+			List<Product> products = productService.viewAllProducts();
+			if(products.isEmpty()) {
+				return new Response(500,"No products available");
+			}
+			return new Response(200,products.stream().map(Product::toString).collect(Collectors.joining("\n")));
 		}
-		return new Response(200,products.stream().map(Product::toString).collect(Collectors.joining("\n")));
+		catch(RetailException e) {
+			return new Response(504,"Fetching product details failed "+e.getMessage());
+		}
 	}
 	
 	public Response viewProductsByCategory(String category) {
 //      Perform category validation
-        List<Product> products = productService.viewProductsByCategory(category);
-        if(products.isEmpty()) {
-			return new Response(500,"No products available for the category");
+		try {
+			List<Product> products = productService.viewProductsByCategory(category);
+	        if(products.isEmpty()) {
+				return new Response(500,"No products available for the category");
+			}
+	        return new Response(200,products.stream().map(Product::toString).collect(Collectors.joining("\n")));
 		}
-        return new Response(200,products.stream().map(Product::toString).collect(Collectors.joining("\n"))); 
+		catch(RetailException e) {
+			return new Response(504,"Fetching products failed "+e.getMessage());
+		}
+         
     }
+
+	public Response sortProductsInAscending() {
+		Comparator<Product> comparator=Comparators.priceAscending;
+		try {
+			List<Product> sortedProducts = productService.sortProducts(comparator);
+	        if(sortedProducts.isEmpty()) {
+				return new Response(500,"No products available");
+			}
+	        return new Response(200,sortedProducts.stream().map(Product::toString).collect(Collectors.joining("\n")));
+		}
+	    catch(RetailException e) {
+			return new Response(504,"Product sorting failed "+e.getMessage());
+        }
+	}
 	
-	public Response sortAndDisplayProducts(Comparator<Product> comparator) {
-		// If no comparator is provided, return empty list
-        List<Product> sortedProducts = productService.sortProducts(comparator);
-        if(sortedProducts.isEmpty()) {
-			return new Response(500,"No products available");
+	public Response sortProductsInDescending() {
+		Comparator<Product> comparator=Comparators.priceDescending;
+		try {
+			List<Product> sortedProducts = productService.sortProducts(comparator);
+	        if(sortedProducts.isEmpty()) {
+				return new Response(500,"No products available");
+			}
+	        return new Response(200,sortedProducts.stream().map(Product::toString).collect(Collectors.joining("\n")));
 		}
-        return new Response(200,sortedProducts.stream().map(Product::toString).collect(Collectors.joining("\n")));
-    }
+	    catch(RetailException e) {
+			return new Response(504,"Product sorting failed "+e.getMessage());
+        }
+	}
+	
+	public Response sortProductsByNamesInAscending() {
+		Comparator<Product> comparator=Comparators.nameAscending;
+		try {
+			List<Product> sortedProducts = productService.sortProducts(comparator);
+	        if(sortedProducts.isEmpty()) {
+				return new Response(500,"No products available");
+			}
+	        return new Response(200,sortedProducts.stream().map(Product::toString).collect(Collectors.joining("\n")));
+		}
+	    catch(RetailException e) {
+			return new Response(504,"Product sorting failed "+e.getMessage());
+        }
+	}
+	
+	public Response sortProductsByNamesInDescending() {
+		Comparator<Product> comparator=Comparators.nameDescending;
+		try {
+			List<Product> sortedProducts = productService.sortProducts(comparator);
+	        if(sortedProducts.isEmpty()) {
+				return new Response(500,"No products available");
+			}
+	        return new Response(200,sortedProducts.stream().map(Product::toString).collect(Collectors.joining("\n")));
+		}
+	    catch(RetailException e) {
+			return new Response(504,"Product sorting failed "+e.getMessage());
+        }
+        
+	}
 }
 
