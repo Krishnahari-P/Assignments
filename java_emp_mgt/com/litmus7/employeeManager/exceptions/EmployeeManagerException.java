87cedb2577d486e7aac7921f529cf5f72b234bff
Krishnahari P <krishnaharip707@gmail.com>
2025-08-20 17:25:40 +0530

    Updated custom exception to include error codes

--- java_emp_mgt/com/litmus7/employeeManager/exceptions/EmployeeManagerException.java
+++ java_emp_mgt/com/litmus7/employeeManager/exceptions/EmployeeManagerException.java
@@ -1,12 +1,29 @@
 package com.litmus7.employeeManager.exceptions;
 
+import com.litmus7.employeeManager.constants.ErrorCode;
+
 public class EmployeeManagerException extends Exception {
+	private final ErrorCode errorCode;
 	
-	public EmployeeManagerException(String errorMessage) {
-		super(errorMessage);
-	}
+//	public EmployeeManagerException(String errorMessage) {
+//		super(errorMessage);
+//	}
+//	
+//	public EmployeeManagerException(String errorMessage,Throwable cause) {
+//		super(errorMessage,cause);
+//	}
 	
-	public EmployeeManagerException(String errorMessage,Throwable cause) {
-		super(errorMessage,cause);
-	}
+	public EmployeeManagerException(ErrorCode errorCode) {
+        super(errorCode.getMessage()); 
+        this.errorCode = errorCode;
+    }
+	
+	public EmployeeManagerException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getMessage(), cause);
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
 }
