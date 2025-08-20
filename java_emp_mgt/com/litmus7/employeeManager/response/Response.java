47aca1f7e37aa5feedcd4f0032879434d08c66af
Krishnahari P <krishnaharip707@gmail.com>
2025-08-20 17:24:40 +0530

    Updated response to include error codes

--- java_emp_mgt/com/litmus7/employeeManager/response/Response.java
+++ java_emp_mgt/com/litmus7/employeeManager/response/Response.java
@@ -1,12 +1,27 @@
 package com.litmus7.employeeManager.response;
 
-public class Response {
+import com.litmus7.employeeManager.constants.ErrorCode;
+
+public class Response<T> {
 	int statusCode;
 	String message;
+	private T data;
 	public Response(int statusCode,String message) {
 		this.statusCode=statusCode;
 		this.message=message;
 	}
+	
+	public Response(int statusCode,String message, T data) {
+		this.statusCode=statusCode;
+		this.message=message;
+		this.data=data;
+	}
+	
+	public Response(ErrorCode errorCode) {
+		this.statusCode=errorCode.getCode();
+		this.message=errorCode.getMessage();
+	}
+	
 	
 	public int getStatusCode() {
 		return statusCode;
@@ -14,5 +29,9 @@ public class Response {
 	
 	public String getMessage() {
 		return message;
+	}
+	
+	public T getData() {
+		return data;
 	}
 }
